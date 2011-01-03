cf6bd130888d00b867e47923a15ed7c3b9e1886a
Grégory PAUL <paulgreg@gmail.com>
2011-01-03 21:19:18 +0100

    fix offline demo

--- exemples/offline/offline.js
+++ exemples/offline/offline.js
@@ -1,18 +1,18 @@
 window.addEventListener("load", function(e) { 
+
 	document.getElementById('networkStatus').innerHTML = 'navigator.onLine=' + navigator.onLine;
 
 	window.applicationCache.update(); // Try to update cache
 	if (window.applicationCache.status = window.UPDATEREADY)
 		window.applicationCache.swapCache(); // Swap cache !
 
-}, false);
+	document.body.addEventListener("online", function(e) { 
+		document.getElementById('networkStatus').innerHTML = 'online';
+	}, false);
 
+	document.body.addEventListener("offline", function(e) { 
+		document.getElementById('networkStatus').innerHTML = 'offline';
+	}, false);
 
-window.addEventListener("online", function(e) { 
-	document.getElementById('networkStatus').innerHTML = 'online';
-}, false);
-
-window.addEventListener("offline", function(e) { 
-	document.getElementById('networkStatus').innerHTML = 'offline';
 }, false);
 
